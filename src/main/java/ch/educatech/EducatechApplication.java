cdf7dc7420dd4115ec09ce14afa25ed9e5a348ce
yves <aucheryv@aucheryv-XPS-13-9343>
2015-11-20 17:43:52 +0100

    first app

---
+++ src/main/java/ch/educatech/EducatechApplication.java
@@ -0,0 +1,12 @@
+package ch.educatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducatechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducatechApplication.class, args);
+    }
+}
