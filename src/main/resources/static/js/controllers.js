2e3291a64ac820c26627df6a1f615606c5000c4d
yves <aucheryv@aucheryv-XPS-13-9343>
2015-11-21 15:07:10 +0100

    commit4

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -6,7 +6,7 @@ var videoControllers = angular.module('videoControllers', []);
 
 videoControllers.controller('categoryListCtrl', ['$scope', '$http','$routeParams',
   function($scope, $http,$routeParams) {
-    $http.get('http://educatech.herokuapp.com/ws/category/'+$routeParams.category).success(function(data) {
+    $http.get('http://localhost:8080/ws/category/'+$routeParams.category).success(function(data) {
       $scope.videos = data;
     });
 
